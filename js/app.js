a44b8c6d45f953eb05d09a78a7c6546631b1e0f3
Jasiameher20 <110532501+Jasiameher20@users.noreply.github.com>
2022-11-19 13:15:01 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+ /* var typed = new Typed("#typed", {
+    // Waits 1000ms after typing "First"
+    strings: ['I am a Web Desighner.', 
+              'I am a Web Developer.',
+              'I am a Full-Stack-Desighner.'],
+              typeSpeed: 300,
+              backSpeed: 50,
+  });*/
+  new Vivus("signature", { duration: 200, type: 'oneByOne' });
+  var mixer = mixitup('.box');
